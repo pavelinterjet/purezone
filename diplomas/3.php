b2dfa800d87bcbad75173d9a92ad9d476e8e1d28
pavelinterjet <79507961+pavelinterjet@users.noreply.github.com>
2021-03-03 10:18:23 +0200

    Add files via upload

---
+++ diplomas/3.php
@@ -0,0 +1,14 @@
+<div class="car_dip">
+    <?php 
+    $path    = 'Press-Release-HEXIS-PureZone-July2020-EN';
+    $files = scandir($path);
+    $path_to_img = 'diplomas';
+
+    $files = array_diff(scandir($path), array('.', '..'));
+    foreach( $files as $file ) {
+    ?>
+        <div class="dip_item">
+            <img src="<?php echo $path_to_img.'/'.$path.'/'.$file; ?>" alt="">
+        </div>
+    <?php } ?>
+</div>
